42ddbb090108e1b061efa5602bb8fbf93e145f11
Hisui02 <alvarolgleiva@gmail.com>
2023-07-26 15:54:29 +0200

    eliminados console.log

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,6 @@ import { useSession, signIn, signOut } from "next-auth/react";
 import { redirect } from "next/navigation";
 export default function landing() {
   const { data: session, status } = useSession({ required: true });
-  //console.log(session);
 
   if (status === "loading") {
     return <>Loading...</>;
